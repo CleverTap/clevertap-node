d67e566c74a2848f7042cf4a14f1a00b70006a5a
abhishek.g <abhishek.g@clevertap.com>
2021-09-10 11:25:33 +0530

    add clevertap endpoint validation changes in the init method

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 const CT_ACCOUNT_ID = "948-4KK-444Z"
 const CT_ACCOUNT_PASSCODE = "QAE-AWB-AAAL"
-const CT_ENDPOINT = "eu1"
+const CT_ENDPOINT = ""
 
 const CleverTap = require('../lib/clevertap');
 const clevertap = CleverTap.init(CT_ACCOUNT_ID, CT_ACCOUNT_PASSCODE,CT_ENDPOINT);
